70d30e762074ae0dae1fbeb0c0fbb89add53583d
EmissarKir <61598599+EmissarKir@users.noreply.github.com>
2021-10-28 22:25:01 +0300

    second commit

--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,40 @@
+import React, { useEffect, useState, useCallback } from "react";
+import PropTypes from "prop-types";
+
+// Компонент LogOutButton
+const LogOutButton = ({ onLogOut }) => {
+    useEffect(() => {
+        console.log("render button");
+    });
+    return (
+        <button className="btn btn-primary" onClick={onLogOut}>
+            LogOut
+        </button>
+    );
+};
+LogOutButton.propTypes = {
+    onLogOut: PropTypes.func
+};
+const MemoizedLogOutMemo = React.memo(LogOutButton);
+
 const MemoWithUseCallbackExample = (props) => {
-    return null;
+    const [state, setState] = useState(false);
+    const handleLogOut = useCallback(() => {
+        localStorage.removeItem("auth");
+    }, []);
+    return (
+        <>
+            <button
+                className="btn btn-primary me-3"
+                onClick={() => {
+                    setState(!state);
+                }}
+            >
+                Initiate rerender
+            </button>
+            <MemoizedLogOutMemo onClick={handleLogOut} />
+        </>
+    );
 };
 
 export default MemoWithUseCallbackExample;
--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const withOutCallback = useRef(0);
+    const withCallback = useRef(0);
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+
+    // with Callback
+    const validateWithCallback = useCallback((data) => {
+        console.log(data);
+    }, []);
+    useEffect(() => {
+        withCallback.current++;
+    }, [validateWithCallback]);
+
+    // withOut Callback
+    const validateWithOutCallback = (data) => {
+        console.log(data);
+    };
+    useEffect(() => {
+        withOutCallback.current++;
+    }, [validateWithOutCallback]);
+
+    useEffect(() => {
+        validateWithOutCallback(data);
+        validateWithCallback(data);
+    }, [data]);
+
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <p>Render withOutCallback: {withCallback.current}</p>
+            <p>Render withCallback: {withOutCallback.current}</p>
+
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                id="name"
+                className="form-control"
+                value={data.email || ""}
+            />
         </CardWrapper>
     );
 };
